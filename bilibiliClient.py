16c094b97c7159a060a38acdcce73f420d323f6e
AD1024 <ccoderad@gmail.com>
2017-02-03 21:45:19 +0800

    modified: text color

--- bilibiliClient.py
+++ bilibiliClient.py
@@ -43,11 +43,11 @@ class bilibiliClient():
         reader, writer = await asyncio.open_connection(self._ChatHost, self._ChatPort)
         self._reader = reader
         self._writer = writer
-        print ('链接弹幕中。。。。。')
+        print ('链接弹幕中...')
         if (await self.SendJoinChannel(self._roomId) == True):
             self.connected = True
-            print ('进入房间成功。。。。。')
-            print ('链接弹幕成功。。。。。')
+            print ('进入房间成功...')
+            print ('链接弹幕成功...')
             await self.ReceiveMessageLoop()
 
     async def HeartbeatLoop(self):
@@ -90,22 +90,21 @@ class bilibiliClient():
 
             if num2 != 0:
                 num -= 1
-                if num==0 or num==1 or num==2:
+                if num == 0 or num == 1 or num == 2:
                     tmp = await self._reader.read(4)
                     num3, = unpack('!I', tmp)
                     print ('房间人数为 %s' % num3)
                     self._UserCount = num3
                     continue
-                elif num==3 or num==4:
+                elif num == 3 or num == 4:
                     tmp = await self._reader.read(num2)
-                    # strbytes, = unpack('!s', tmp)
                     try: # 为什么还会出现 utf-8 decode error??????
                         messages = tmp.decode('utf-8')
                     except:
                         continue
                     self.parseDanMu(messages)
                     continue
-                elif num==5 or num==6 or num==7:
+                elif num == 5 or num == 6 or num == 7:
                     tmp = await self._reader.read(num2)
                     continue
                 else:
@@ -132,11 +131,11 @@ class bilibiliClient():
             isAdmin = dic['info'][2][2] == '1'
             isVIP = dic['info'][2][3] == '1'
             if isAdmin:
-                commentUser = '管理员 ' + commentUser
+                commentUser = '[管理员] ' + commentUser
             if isVIP:
-                commentUser = 'VIP ' + commentUser
+                commentUser = '[VIP] ' + commentUser
             try:
-                print (commentUser + ' say: ' + commentText)
+                print (commentUser + ': ' + commentText)
             except:
                 pass
             return
@@ -146,14 +145,16 @@ class bilibiliClient():
             Giftrcost = dic['data']['rcost']
             GiftNum = dic['data']['num']
             try:
-                print(GiftUser + ' 送出了 ' + str(GiftNum) + ' 个 ' + GiftName)
+                # print()
+                print('\033[1;33m ' + GiftUser + ' \033[0m!' + ' 送出了 ' + str(GiftNum) + ' 个' + '\033[1;31m ' + GiftName + ' \033[0m!')
             except:
                 pass
             return
         if cmd == 'WELCOME' and config.TURN_WELCOME == 1:
             commentUser = dic['data']['uname']
             try:
-                print ('欢迎 ' + commentUser + ' 进入房间。。。。')
+                commentUser = '\033[1;31;43m ' + commentUser + '\033[0m!'
+                print ('欢迎 ' + commentUser + ' 进入房间')
             except:
                 pass
             return
